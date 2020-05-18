8a73eb10eafdeb88222c7668ba76194b95047071
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-18 18:21:07 +0200

    added uniqueness to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :email, presence: true
   validates :last_name, presence: true
+  validates :email, uniqueness: true
 end
