6be1809bb3f0fb33b0e9f4e9a400db011964fceb
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-18 16:57:32 +0200

    belongs to and has many done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :videogame
+  has_many :reviews
+
 end
--- app/models/videogame.rb
+++ app/models/videogame.rb
@@ -1,3 +1,4 @@
 class Videogame < ApplicationRecord
   belongs_to :user
+  has_many :bookings
 end
