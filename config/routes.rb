12d79bdb0180660f4e3690a98d26c983d80e692b
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-19 12:53:32 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
   resources :videogames
 
   get 'index', to: 'pages#index'
