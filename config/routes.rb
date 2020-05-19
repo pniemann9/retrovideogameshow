4d343ef3d6f246c664f1affe4701d1e8faca5163
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-05-19 13:07:57 +0200

    status

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   resources :videogames
 
-  get 'index', to: 'pages#index'
+  root 'pages#home'
 # verb 'path', to: 'controller#action'
 
 end
