0878394f846dcdd687a91c5762be5c62dc2c784c
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-19 14:28:11 +0200

    h

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone_number])
   end
