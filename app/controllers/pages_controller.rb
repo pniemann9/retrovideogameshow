90f784763152ab9d81d4ece7df4e36617c6eb544
Coralie Delozanne <coralie.delozanne@gmail.com>
2020-05-21 12:13:29 +0200

    Created controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @user = current_user.id
+    @userbookings = Booking.where(user_id: @user)
+    @pastbookings = []
+    @currentbookings = []
+    @futurebookings = []
+    @userbookings.each do |userbooking|
+      if userbooking.end_time < Date.today
+        @pastbookings << userbooking
+      elsif userbooking.start_time > Date.today
+        @futurebookings << userbooking
+      else
+        @currentbookings << userbooking
+      end
+    end
+  end
 end
