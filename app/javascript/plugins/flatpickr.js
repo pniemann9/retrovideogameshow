03c15a316f2c0ff7c07f6f86aa866ede938b7fda
Vilouny Phoubandith <vilouny.ph@gmail.com>
2020-05-21 18:12:18 +0200

    flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,15 @@
 
-import flatpickr from 'flatpickr'
+// import flatpickr from 'flatpickr'
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr(".datepicker", {
+// flatpickr(".datepicker", {
+//   "plugins": [new rangePlugin({ input: "#secondRangeInput"})]
+// });
 
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+flatpickr("#range_start", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
 });
