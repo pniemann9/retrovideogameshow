23be61d16e6686f8c3895071aa03b41e4afb4dff
Philippe Niemann <pniemann@orange.fr>
2020-05-21 17:45:01 +0200

    videogames with picture trouble

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_132124) do
+ActiveRecord::Schema.define(version: 2020_05_21_131059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_05_20_132124) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "genre"
+    t.string "photo"
     t.index ["user_id"], name: "index_videogames_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require "open-uri"
+
+Videogame.destroy_all
+User.destroy_all
+
 
 puts 'Creating 20 fake User and 20 fake videogames...'
 
@@ -18,14 +23,138 @@ y = 50
   y += 1
 end
 
-i = 1
-while i <= 20
-  videogame = Videogame.new(name: Faker::Game.title, description: "#{Faker::Game.genre} #{Faker::Game.platform}", price: rand(5..50), user_id: i, image: "https://cdn03.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_NewSuperMarioBrosUDeluxe_image1600w.jpg")
-  videogame.save!
-  i += 1
+games = [
+  {
+    name:       'The Legend of Zelda : Ocarina of Time',
+    genre:      'Action-Aventure',
+    price:      '7€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590062933/retro%20game%20show/ocarina.jpg',
+  },
+  {
+    name:       'Super Mario Bros.',
+    genre:      'Plateforme',
+    price:      '3€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590071287/retro%20game%20show/mariobros_po5qu2.jpg'
+  },
+  {
+    name:       'Pokémon Rouge',
+    genre:      'Jeu de rôle',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590070099/retro%20game%20show/pkmrouge_kkmswj.jpg'
+  },
+  {
+    name:       'Tomb Raider',
+    genre:      'Plateforme',
+    price:      '4€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590073773/retro%20game%20show/tombraider_i9uykz.jpg'
+  },
+  {
+    name:       'Mario Kart 64',
+    genre:      'Course',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590070298/retro%20game%20show/mariokart64_n3czeu.jpg'
+  },
+  {
+    name:       'Tekken 3',
+    genre:      'Combat',
+    price:      '6€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590072484/retro%20game%20show/taken3_jpelza.jpg'
+  },
+  {
+    name:       'Age of Empires',
+    genre:      'Stratégie temps réel',
+    price:      '2€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590072743/retro%20game%20show/AgeofEmpires_ov5cb5.png'
+  },
+  {
+    name:       'Gran Turismo',
+    genre:      'Course',
+    price:      '3€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590073431/retro%20game%20show/Gran_Turismo_bi5433.png'
+  },
+  {
+    name:       'Sonic the Hedgehog',
+    genre:      'Plateforme',
+    price:      '4€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590070688/retro%20game%20show/sonic2_zyeylx.jpg'
+  },
+  {
+    name:       'Donkey Kong Country',
+    genre:      'Plateforme',
+    price:      '3€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590070718/retro%20game%20show/donkeykongcountry_s88nys.jpg'
+  },
+  {
+    name:       'Final Fantasy VI',
+    genre:      'Jeu de rôle',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590071154/retro%20game%20show/ff7_zkh7uj.jpg'
+  },
+  {
+    name:       'Tetris',
+    genre:      'Réflexion',
+    price:      '1€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590072265/retro%20game%20show/tetris.jpg_xpod5e.jpg'
+  },
+  {
+    name:       'Super Street Fighter II',
+    genre:      'Combat',
+    price:      '6€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590072133/retro%20game%20show/sf2_wr1aaq.jpg'
+  },
+  {
+    name:       'Super Mario Land .',
+    genre:      'Plateforme',
+    price:      '2€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590062752/retro%20game%20show/super-mario-land-3402-diaporama_zgn2e5.jpg'
+  },
+  {
+    name:       'Super Smash Bros',
+    genre:      'Combat',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590071006/retro%20game%20show/ssb_vo6y1y.jpg'
+  },
+  {
+    name:       'Pac-Man',
+    genre:      'Arcade et labyrinthe',
+    price:      '1€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590071588/retro%20game%20show/pacman_i2ydgp.jpg'
+  },
+  {
+    name:       'Grand Theft Auto',
+    genre:      'Action',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590071614/retro%20game%20show/gta_keruus.jpg'
+  },
+  {
+    name:       'F-Zero',
+    genre:      'Course',
+    price:      '5€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590070917/retro%20game%20show/fzero_ioyuvh.jpg'
+  },
+  {
+    name:       'Golden Sun',
+    genre:      'Jeu de rôle',
+    price:      '6€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590062748/retro%20game%20show/game-boy-advance-golden-sun_bepwmj.jpg'
+  },
+  {
+    name:       'Fire emblem : Rekka no ken',
+    genre:      'Jeu de rôle',
+    price:      '3€',
+    photo:      'https://res.cloudinary.com/dubgqyx31/image/upload/v1590063020/retro%20game%20show/781_q3koko.jpg'
+  }
+]
+
+user = User.create(email:"gamer@gmail.com", password:"A12345", first_name: "yoshi", last_name:"nintendo", address:"nintendoland", phone_number:"01234567", avatar:"http://www.pngall.com/wp-content/uploads/2/Video-Game-PNG-Pic.png")
+user.save!
+puts user
+games.each do |game|
+
+  newgame = Videogame.new(game)
+  newgame.user = user
+  newgame.save!
 end
 
- User.create(email:"gamer@gmail.com", password:"A12345", first_name: "yoshi", last_name:"nintendo", address:"nintendoland", phone_number:"01234567", avatar:"http://www.pngall.com/wp-content/uploads/2/Video-Game-PNG-Pic.png")
 
 puts 'Finished!'
-
